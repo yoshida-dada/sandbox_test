0df2ffc2e483f08143e5cce7f25067e16433b57e
yoshida-dada <102651665+yoshida-dada@users.noreply.github.com>
2022-04-01 09:35:11 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const name = "だだ";
+const age = "22";
+
+const message2 = `私の名前は${name}です。年齢は${age}です`;
+
+console.log(message2);
+
+const func1 = function (str) {
+  return str;
+};
+
+console.log(func1("func1です"));
+
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
